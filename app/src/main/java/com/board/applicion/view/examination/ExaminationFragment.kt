e14b27750a7c33fbd5899d2f054fd8bce47629af
zhangan <zhangan115@gmail.com>
2018-11-17 22:59:19 +0800

    创建项目

--- app/src/main/java/com/board/applicion/view/examination/ExaminationFragment.kt
+++ app/src/main/java/com/board/applicion/view/examination/ExaminationFragment.kt
@@ -1,5 +1,6 @@
 package com.board.applicion.view.examination
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import com.board.applicion.R
@@ -7,9 +8,13 @@ import com.board.applicion.app.App
 import com.board.applicion.base.BaseFragment
 import com.board.applicion.mode.DatabaseStore
 import com.board.applicion.mode.databases.Substation
+import com.board.applicion.view.examination.room.CabinetListActivity
 import kotlinx.android.synthetic.main.fragment_examination.*
 
 class ExaminationFragment : BaseFragment() {
+
+    lateinit var adapter: SubListAdapter
+    private val dataList = ArrayList<Substation>()
 
     companion object {
         fun getFragment(): ExaminationFragment {
@@ -23,15 +28,30 @@ class ExaminationFragment : BaseFragment() {
     override fun getContentView(): Int {
         return R.layout.fragment_examination
     }
-    private lateinit var subStore : DatabaseStore<Substation>
-    override fun initData() {
-        subStore = DatabaseStore(lifecycle,Substation::class.java)
-        subStore.getQueryData(subStore.getQueryBuilder().build()){
 
+    private lateinit var subStore: DatabaseStore<Substation>
+    override fun initData() {
+        subStore = DatabaseStore(lifecycle, Substation::class.java)
+        subStore.getQueryData(subStore.getQueryBuilder().build()) {
+            dataList.clear()
+            dataList.addAll(it)
+            adapter.notifyDataSetChanged()
+            for (i in 0 until expandableListView.count) {
+                expandableListView.expandGroup(i)
+            }
         }
     }
 
     override fun initView() {
         userText.text = "欢迎，${App.instance.getCurrentUser().realName}"
+        adapter = SubListAdapter(activity, R.layout.item_sub, R.layout.item_room)
+        adapter.setData(dataList)
+        adapter.setItemListener {
+            val intent = Intent(activity, CabinetListActivity::class.java)
+            intent.putExtra("title", it.name)
+            intent.putExtra("id", it.id)
+            startActivity(intent)
+        }
+        expandableListView.setAdapter(adapter)
     }
 }
---
+++ app/src/main/java/com/board/applicion/view/examination/room/CabinetListActivity.kt
@@ -0,0 +1,84 @@
+package com.board.applicion.view.examination.room
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.board.applicion.R
+import com.board.applicion.base.BaseActivity
+import com.board.applicion.mode.DatabaseStore
+import com.board.applicion.mode.databases.Cabinet
+import com.board.applicion.mode.databases.Cabinet_
+import kotlinx.android.synthetic.main.activity_cabinet_list.*
+
+class CabinetListActivity : BaseActivity() {
+
+    var dataList = ArrayList<Cabinet>()
+
+    override fun initView(savedInstanceState: Bundle?) {
+        recycleView.layoutManager = LinearLayoutManager(this)
+        val headerView = LayoutInflater.from(this).inflate(R.layout.layout_search_all, null)
+        recycleView.addHeaderView(headerView)
+        headerView.setOnClickListener {
+            //to search
+        }
+        recycleView.adapter = Adapter(dataList, this)
+    }
+
+    override fun initData() {
+        val roomId = intent.getLongExtra("id", -1)
+        val cabinetStore = DatabaseStore<Cabinet>(lifecycle, Cabinet::class.java)
+        cabinetStore.getQueryData(cabinetStore.getQueryBuilder().equal(Cabinet_.mcrId, roomId).build()) {
+            dataList.clear()
+            dataList.addAll(it)
+            if (dataList.isEmpty()) {
+                noDataTv.visibility = View.VISIBLE
+            } else {
+                noDataTv.visibility = View.GONE
+            }
+            recycleView.adapter?.notifyDataSetChanged()
+        }
+    }
+
+    override fun getContentView(): Int {
+        return R.layout.activity_cabinet_list
+    }
+
+    override fun getToolBarTitle(): String? {
+        return intent.getStringExtra("title")
+    }
+
+
+    private class Adapter(private val dataList: ArrayList<Cabinet>, private val content: Context)
+        : RecyclerView.Adapter<ViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            val view = LayoutInflater.from(content).inflate(R.layout.item_cabinet, parent, false)
+            val name = view.findViewById<TextView>(R.id.name)
+            return ViewHolder(view, name)
+        }
+
+        override fun getItemCount(): Int {
+            return dataList.size
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            holder.name.text = dataList[position].name
+            holder.itemView.setOnClickListener {
+                if (content is Activity) {
+                    content.startActivity(Intent())
+                }
+            }
+        }
+    }
+
+    private class ViewHolder(itemView: View, val name: TextView)
+        : RecyclerView.ViewHolder(itemView)
+
+}
