fc1a9663d8c9ecfead7b783c6d004a014bb96052
zhangan <zhangan115@126.com>
2018-11-17 21:11:44 +0800

    增加

--- app/src/main/java/com/board/applicion/view/examination/ExaminationFragment.kt
+++ app/src/main/java/com/board/applicion/view/examination/ExaminationFragment.kt
@@ -1,9 +1,12 @@
 package com.board.applicion.view.examination
 
 import android.os.Bundle
+import android.util.Log
 import com.board.applicion.R
 import com.board.applicion.app.App
 import com.board.applicion.base.BaseFragment
+import com.board.applicion.mode.DatabaseStore
+import com.board.applicion.mode.databases.Substation
 import kotlinx.android.synthetic.main.fragment_examination.*
 
 class ExaminationFragment : BaseFragment() {
@@ -20,9 +23,12 @@ class ExaminationFragment : BaseFragment() {
     override fun getContentView(): Int {
         return R.layout.fragment_examination
     }
-
+    private lateinit var subStore : DatabaseStore<Substation>
     override fun initData() {
+        subStore = DatabaseStore(lifecycle,Substation::class.java)
+        subStore.getQueryData(subStore.getQueryBuilder().build()){
 
+        }
     }
 
     override fun initView() {
