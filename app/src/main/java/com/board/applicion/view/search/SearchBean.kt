e895c75b4208c1aea6785a8d80193f85b39a84aa
zhangan <zhangan115@gmail.com>
2018-12-12 23:01:58 +0800

    项目资源

--- app/src/main/java/com/board/applicion/view/search/SearchBean.kt
+++ app/src/main/java/com/board/applicion/view/search/SearchBean.kt
@@ -1,5 +1,5 @@
 package com.board.applicion.view.search
 
-data class SearchCondition(var id: Long, var name: String, var type: Int,var resultList:List<SearchResult>)
+data class SearchCondition( var name: String, var type: Int,var resultList:List<SearchResult>)
 
 data  class SearchResult(var id:Long,var name:String)
--- app/src/main/java/com/board/applicion/view/search/SearchFragment.kt
+++ app/src/main/java/com/board/applicion/view/search/SearchFragment.kt
@@ -1,18 +1,16 @@
 package com.board.applicion.view.search
 
-import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.Animation
-import android.view.animation.AnimationSet
 import android.view.animation.AnimationUtils
-import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
@@ -20,88 +18,18 @@ import com.board.applicion.R
 import com.board.applicion.base.BaseFragment
 import com.board.applicion.mode.DatabaseStore
 import com.board.applicion.mode.User
-import com.board.applicion.mode.databases.Cabinet
-import com.board.applicion.mode.databases.MainControlRoom
-import com.board.applicion.mode.databases.Substation
+import com.board.applicion.mode.User_
+import com.board.applicion.mode.databases.*
 import com.board.applicion.view.examination.SubListAdapter
 import com.board.applicion.view.examination.room.CabinetListActivity
 import kotlinx.android.synthetic.main.fragment_search.*
 
 class SearchFragment : BaseFragment(), View.OnClickListener {
 
-    override fun onClick(v: View?) {
-        when (v?.id) {
-            R.id.chooseSubLayout -> {
-
-            }
-            R.id.chooseRoomLayout -> {
-
-            }
-            R.id.chooseCabinetLayout -> {
-
-            }
-            R.id.chooseUserLayout -> {
-
-            }
-        }
-
-    }
-
-    private var searchResultList: ArrayList<SearchResult>? = null
-    private var searchCondition: SearchCondition? = null
-    private val animTime = 1500L
-
-    private fun showSearchResult(searchCondition: SearchCondition) {
-        this.searchCondition = searchCondition
-        searchResultList?.clear()
-        searchResultList?.addAll(searchCondition.resultList)
-        recycleView.adapter?.notifyDataSetChanged()
-        this.showOpenAnim()
-    }
-
-    private fun showOpenAnim() {
-        val tranAnim = ObjectAnimator.ofFloat(searchLayout,"translationX",0F,-viewWidth!!.toFloat())
-        val resultAnim = ObjectAnimator.ofFloat(resultLayout,"translationX",viewWidth!!.toFloat(),0F)
-        searchLayout.visibility = View.GONE
-        resultLayout.visibility = View.VISIBLE
-    }
-
-    private fun showCloseAnim() {
-        val closeAnim = AnimationUtils.loadAnimation(activity, R.anim.close_search)
-        closeAnim.duration = animTime
-        closeAnim.fillAfter = true
-        closeAnim.setAnimationListener(object : Animation.AnimationListener {
-            override fun onAnimationRepeat(animation: Animation?) {
-
-            }
-
-            override fun onAnimationEnd(animation: Animation?) {
-
-            }
-
-            override fun onAnimationStart(animation: Animation?) {
-                searchLayout.visibility = View.VISIBLE
-            }
-
-        })
-        searchLayout.startAnimation(closeAnim)
-        val closeResultAnim = AnimationUtils.loadAnimation(activity, R.anim.close_result)
-        closeResultAnim.duration = animTime
-        closeResultAnim.fillAfter = true
-        closeResultAnim.setAnimationListener(object : Animation.AnimationListener {
-            override fun onAnimationRepeat(animation: Animation?) {
-
-            }
-
-            override fun onAnimationEnd(animation: Animation?) {
-                resultLayout.visibility = View.GONE
-            }
-
-            override fun onAnimationStart(animation: Animation?) {
-            }
-        })
-        resultLayout.startAnimation(closeResultAnim)
-    }
+    var subList: ArrayList<Substation> = ArrayList()
+    var roomList: ArrayList<MainControlRoom> = ArrayList()
+    var cabinetList: ArrayList<Cabinet> = ArrayList()
+    var userList: ArrayList<User> = ArrayList()
 
     private var isFilter = false
     //search data
@@ -115,6 +43,10 @@ class SearchFragment : BaseFragment(), View.OnClickListener {
     private lateinit var subStore: DatabaseStore<Substation>
     lateinit var adapter: SubListAdapter
     private val dataList = ArrayList<Substation>()
+    private var conditionAdapter: Adapter? = null
+
+    private var searchResultList: ArrayList<SearchResult>? = null
+    private var searchCondition: SearchCondition? = null
 
     companion object {
         fun getFragment(): SearchFragment {
@@ -123,6 +55,92 @@ class SearchFragment : BaseFragment(), View.OnClickListener {
             fragment.arguments = bundle
             return fragment
         }
+    }
+
+    override fun onClick(v: View?) {
+        searchResultList?.clear()
+        when (v?.id) {
+            R.id.chooseSubLayout -> {
+                subList.clear()
+                subList.addAll(subStore.getQueryBuilder().equal(Substation_.status, 0).build().find())
+                if (subList.isEmpty()) return
+                for (item in subList) {
+                    searchResultList?.add(SearchResult(item.id, item.name))
+                }
+                if (substation != null) {
+                    conditionAdapter?.chooseId = substation!!.id
+                }
+                searchCondition = SearchCondition("屏柜", 1, searchResultList!!)
+            }
+            R.id.chooseRoomLayout -> {
+                if (substation == null) return
+                roomList.clear()
+
+                roomList.addAll(DatabaseStore(lifecycle, MainControlRoom::class.java).getQueryBuilder()
+                        .equal(MainControlRoom_.subId, substation!!.id)
+                        .equal(MainControlRoom_.status, 0)
+                        .build().find())
+                if (roomList.isEmpty()) return
+                for (item in roomList) {
+                    searchResultList?.add(SearchResult(item.id, item.name))
+                }
+                if (mainRoom != null) {
+                    conditionAdapter?.chooseId = mainRoom!!.id
+                }
+                searchCondition = SearchCondition("主控室", 2, searchResultList!!)
+            }
+            R.id.chooseCabinetLayout -> {
+                if (mainRoom == null) return
+                cabinetList.clear()
+                cabinetList.addAll(DatabaseStore(lifecycle, Cabinet::class.java).getQueryBuilder()
+                        .equal(Cabinet_.mcrId, mainRoom!!.id)
+                        .equal(Cabinet_.status, 0)
+                        .build().find())
+                if (cabinetList.isEmpty()) return
+                for (item in cabinetList) {
+                    searchResultList?.add(SearchResult(item.id, item.name))
+                }
+                if (cabinet != null) {
+                    conditionAdapter?.chooseId = cabinet!!.id
+                }
+                searchCondition = SearchCondition("屏柜", 3, searchResultList!!)
+            }
+            R.id.chooseUserLayout -> {
+                userList.clear()
+                userList.addAll(DatabaseStore(lifecycle, User::class.java).getQueryBuilder()
+                        .equal(User_.status, 0)
+                        .build().find())
+                if (userList.isEmpty()) return
+                for (item in userList) {
+                    searchResultList?.add(SearchResult(item.id, item.realName))
+                }
+                if (checkUser != null) {
+                    conditionAdapter?.chooseId = checkUser!!.id
+                }
+                searchCondition = SearchCondition("检查人员", 4, searchResultList!!)
+            }
+        }
+        if (this.searchCondition == null) return
+        showSearchResult()
+    }
+
+    private fun showSearchResult() {
+        if (searchCondition == null) return
+        recycleView.adapter?.notifyDataSetChanged()
+        if (searchCondition != null) {
+            titleText.text = searchCondition!!.name
+        }
+        this.showOpenAnim()
+    }
+
+    private fun showOpenAnim() {
+        searchLayout.visibility = View.GONE
+        resultLayout.visibility = View.VISIBLE
+    }
+
+    private fun showCloseAnim() {
+        searchLayout.visibility = View.VISIBLE
+        resultLayout.visibility = View.GONE
     }
 
     override fun getContentView(): Int {
@@ -139,7 +157,6 @@ class SearchFragment : BaseFragment(), View.OnClickListener {
             isFilter = !isFilter
             if (isFilter) {
                 filterLayout.visibility = View.VISIBLE
-
             } else {
                 filterLayout.visibility = View.GONE
             }
@@ -151,14 +168,15 @@ class SearchFragment : BaseFragment(), View.OnClickListener {
         chooseStartLayout.setOnClickListener {
 
         }
+        recycleView.layoutManager = GridLayoutManager(activity, 1)
+        conditionAdapter = Adapter(searchResultList!!, activity!!)
+        recycleView.adapter = conditionAdapter
         chooseEndLayout.setOnClickListener {
 
         }
         viewWidth = activity?.resources?.displayMetrics?.widthPixels
-//        filterLayout.layoutParams = FrameLayout.LayoutParams(viewWidth!!*2,LinearLayout.LayoutParams.WRAP_CONTENT)
-        searchLayout.layoutParams = LinearLayout.LayoutParams(viewWidth!!,LinearLayout.LayoutParams.WRAP_CONTENT)
+        searchLayout.layoutParams = LinearLayout.LayoutParams(viewWidth!!, LinearLayout.LayoutParams.WRAP_CONTENT)
         resultLayout.layoutParams = LinearLayout.LayoutParams(viewWidth!!, LinearLayout.LayoutParams.WRAP_CONTENT)
-        recycleView.adapter = Adapter(searchResultList!!, activity!!)
         adapter = SubListAdapter(activity, R.layout.item_sub, R.layout.item_room)
         adapter.setData(dataList)
         adapter.setItemListener {
@@ -183,6 +201,12 @@ class SearchFragment : BaseFragment(), View.OnClickListener {
             }
         }
         resetBtn.setOnClickListener {
+            substation = null
+            subList.clear()
+            roomList.clear()
+            cabinetList.clear()
+            mainRoom = null
+            cabinet = null
             filterUIUpdate()
         }
         backSearchBtn.setOnClickListener {
@@ -191,10 +215,74 @@ class SearchFragment : BaseFragment(), View.OnClickListener {
     }
 
     private fun filterUIUpdate() {
-
+        if (substation == null) {
+            subName.text = ""
+        } else {
+            subName.text = substation!!.name
+        }
+        if (mainRoom == null) {
+            roomName.text = ""
+        } else {
+            roomName.text = mainRoom!!.name
+        }
+        if (cabinet == null) {
+            cabinetName.text = ""
+        } else {
+            cabinetName.text = cabinet!!.name
+        }
+        if (checkUser == null) {
+            userName.text = ""
+        } else {
+            userName.text = checkUser!!.realName
+        }
     }
 
-    private class Adapter(private val dataList: ArrayList<SearchResult>, private val content: Context)
+    private fun itemClick(result: SearchResult) {
+        when (searchCondition?.type) {
+            1 -> {
+                for (item in subList) {
+                    if (result.id == item.id) {
+                        substation = item
+                        break
+                    }
+                }
+                roomList.clear()
+                cabinetList.clear()
+                mainRoom = null
+                cabinet = null
+            }
+            2 -> {
+                for (item in roomList) {
+                    if (result.id == item.id) {
+                        mainRoom = item
+                        break
+                    }
+                }
+                cabinetList.clear()
+                cabinet = null
+            }
+            3 -> {
+                for (item in cabinetList) {
+                    if (result.id == item.id) {
+                        cabinet = item
+                        break
+                    }
+                }
+            }
+            4 -> {
+                for (item in userList) {
+                    if (result.id == item.id) {
+                        checkUser = item
+                        break
+                    }
+                }
+            }
+        }
+        filterUIUpdate()
+        showCloseAnim()
+    }
+
+    private inner class Adapter(private val dataList: ArrayList<SearchResult>, private val content: Context)
         : RecyclerView.Adapter<ViewHolder>() {
         var chooseId: Long = -1
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -216,7 +304,7 @@ class SearchFragment : BaseFragment(), View.OnClickListener {
                 holder.imageView.visibility = View.INVISIBLE
             }
             holder.itemView.setOnClickListener {
-
+                itemClick(dataList[position])
             }
         }
     }
